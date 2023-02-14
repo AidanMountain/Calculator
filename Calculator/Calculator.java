9eaa43edf2ce696600ff3cf656cb0026f330f8aa
AidanMountain <34258373+AidanMountain@users.noreply.github.com>
2023-02-14 16:55:20 -0500

    Aidan

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -2,5 +2,8 @@ public class Calculator {
     public static void main(String[] args){
         System.out.println("2 + 2 = 5");
         System.out.println("This is not a real calculator");
+        System.out.println("I am not sure if I need to be making a real calculator or if I can jsut be pushing random things");
+        int x = 1000;
+        double y = 2.00;
     }
 }

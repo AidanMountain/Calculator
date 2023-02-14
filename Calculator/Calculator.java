558d9b899f7994c9f42e7eb1c40922176b16b529
AidanMountain <34258373+AidanMountain@users.noreply.github.com>
2023-02-14 16:53:19 -0500

    I am not sure if I actually have to be making a calculator

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -1,5 +1,6 @@
 public class Calculator {
     public static void main(String[] args){
         System.out.println("2 + 2 = 5");
+        System.out.println("This is not a real calculator");
     }
 }
